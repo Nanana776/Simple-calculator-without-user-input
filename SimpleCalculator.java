f17e0287944fe3d52d0dbf0697e26a4059295f58
Nanana776 <nanabudaghyan06@gmail.com>
2024-11-17 01:39:49 +0400

    simple calculator

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,25 +1,4 @@
 public class SimpleCalculator {
-
-    public static void main(String[] args) {
-        
-        double num1=10;
-        double num2=5;
-        
-        double sum=add(num1, num2);
-        double difference=subtract(num1, num2);
-        double product=multiply(num1, num2);
-        double quotient=divide(num1, num2);
-        double power=pow(num1, num2);
-        double root=sqrt(num1);
-
-        System.out.println("Addition: " + sum);
-        System.out.println("Subtraction: " + difference);
-        System.out.println("Multiplication: " + product);
-        System.out.println("Division: " + quotient);
-        System.out.println("Power: " + power);
-        System.out.println("Square root: " + root);
-    }
-    
     
     public static double add(double a, double b) {
         return a+b;
@@ -53,5 +32,24 @@ public class SimpleCalculator {
         return Math.pow(num1, 1.0/2);
     }
 
+    public static void main(String[] args) {
+        
+        double num1=10;
+        double num2=5;
+        
+        double sum=add(num1, num2);
+        double difference=subtract(num1, num2);
+        double product=multiply(num1, num2);
+        double quotient=divide(num1, num2);
+        double power=pow(num1, num2);
+        double root=sqrt(num1);
+
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + quotient);
+        System.out.println("Power: " + power);
+        System.out.println("Square root: " + root);
+    }
     
 }
